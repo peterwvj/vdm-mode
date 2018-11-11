5440ca997b997df11d3d3bf67e4b547df6df118d
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2018-11-11 15:32:58 +0100

    Add REPL support for vdm-mode

--- vdm-mode.el
+++ vdm-mode.el
@@ -30,9 +30,9 @@
 ;; - Replacement of ASCII syntax (e.g. lambda) with more aesthetically
 ;;   looking symbols (e.g. λ) using prettify-symbols-mode
 ;; - On the fly syntax checking using Flycheck
-;;   - Integration with VDMJ and Overture
 ;; - VDM YASnippets
-
+;; - REPL (read–eval–print loop) support based on comint
+;; - Integration with VDMJ and Overture
 
 ;;; Code:
 
