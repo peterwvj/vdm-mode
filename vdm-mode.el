c4bd27d8a1232bb576747918f7e4f2dc7eca61db
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2019-02-13 17:49:15 +0100

    Prettify range restriction

--- vdm-mode.el
+++ vdm-mode.el
@@ -90,7 +90,7 @@
    ("==" . ?≜) ("in set" . ?∈) ("not in set" . ?∉) ("<>" . ?≠) ("<=" . ?≤)
    (">=" . ?≥) ("|->" . ?↦) ("div" . ?÷) ("subset" . ?⊆) ("undefined" . ?⊢)
    ("->" . ?⭢) ("inter" . ?∩) ("union" . ?∪) ("*" . ?×) ("exists" . ?∃)
-   ("forall" . ?∀) ("lambda" . ?λ) ("++" . ?†) ("<:" . ?◁))
+   ("forall" . ?∀) ("lambda" . ?λ) ("++" . ?†) ("<:" . ?◁) (":>" . ?▷))
   "VDM symbol prettifications.")
 
 (defconst vdm-mode-vdmrt-keywords
