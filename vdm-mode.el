3dcdeec972677c0875b714d4a635edf16e91d803
Peter W. V. Tran-Jørgensen <pvj@eng.au.dk>
2019-02-13 21:15:57 +0100

    Prettify iota

--- vdm-mode.el
+++ vdm-mode.el
@@ -110,7 +110,7 @@ Inspired by https://emacs.stackexchange.com/questions/34808/using-prettify-symbo
    ("->" . ?⭢) ("inter" . ?∩) ("union" . ?∪) ("*" . ?×) ("exists" . ?∃)
    ("forall" . ?∀) ("lambda" . ?λ) ("++" . ?†) ("<:" . ?◁) (":>" . ?▷)
    ("<-:" . ?⩤) (":->" . ?⩥) ("psubset" . ?⊂) ("^" . ?↷) ("dinter" . ?⋂)
-   ("dunion" . ?⋃) ("power" . ?𝓕) ("mu" . ?μ)
+   ("dunion" . ?⋃) ("power" . ?𝓕) ("mu" . ?μ) ("iota" . ?ι)
    ,(vdm-mode-create-prettification-cons "nat1" "ℕ₁")
    ,(vdm-mode-create-prettification-cons "exists1" "∃!"))
   "VDM symbol prettifications.")
